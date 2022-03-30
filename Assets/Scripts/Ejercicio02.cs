0179a4322d0ec3ab8a8f8dcac4098d0b075221ba
LucasRehmann <101809208+LucasRehmann@users.noreply.github.com>
2022-03-30 20:23:03 -0300

    Ejercicios 1 al 5
    read title

---
+++ Assets/Scripts/Ejercicio02.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio02 : MonoBehaviour
+{
+    int num1 = 4;
+    int num2 = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("a. " + (num1 + num2));
+        Debug.Log("b. " + (num1 * num2));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
