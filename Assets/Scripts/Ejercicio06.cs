c28dea53688ce7d25d555d6cf21d7c4cdb8d5f0e
LucasRehmann <101809208+LucasRehmann@users.noreply.github.com>
2022-03-31 10:42:02 -0300

    Ejercicios 6 a 8
    read title again

---
+++ Assets/Scripts/Ejercicio06.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio06 : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1%2 == 0)
+        {
+            Debug.Log("El numero es par");
+        }
+
+        else
+        {
+            Debug.Log("El numero es impar");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
